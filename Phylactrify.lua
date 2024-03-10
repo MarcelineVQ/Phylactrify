61d3fdfb51dbc2bc356170290fdc67182cabd419
MarcelineVQ <marcythevq@gmail.com>
2024-03-09 19:13:55 -0800

    refactor the logic to be generic in slot and item

--- Phylactrify.lua
+++ Phylactrify.lua
@@ -21,8 +21,22 @@ function ItemLinkToName(link)
 	end
 end
 
+function FindInvItemSlot(item)
+	if ( not item ) then return; end
+	item = string.lower(ItemLinkToName(item));
+	local link;
+	for i = 1,23 do
+		link = GetInventoryItemLink("player",i);
+		if ( link ) then
+			if ( item == string.lower(ItemLinkToName(link)) )then
+				return i
+			end
+		end
+	end
+end
+
 -- adapted from supermacros
-local function FindItem(item)
+local function FindBagItem(item)
 	if ( not item ) then return; end
 	item = string.lower(ItemLinkToName(item));
 	local link;
@@ -51,18 +65,19 @@ local defaults =
   enabled = true,
   naxx_only = false,
   do_toggle = false,
-  trink_slot = 13,
+  item_slot = 13,
   announce = true,
+  item_link = "|cffa335ee|Hitem:23206:0:0:0:0:0:0:0:0|h[Mark of the Champion]|h|r",
 }
 
-local phylactryName = "Mark of the Champion"
--- local phylactryName = "Rune of the Guard Captain"
-
-function checkTrinketSlot(slot,trink)
-  local link = GetInventoryItemLink("player",slot)
-  if link then
-    local _,_,itemName,_ = string.find(link,"|h%[?([^%[%]]*)%]|h")
-    if itemName == trink then return slot end
+function checkItemSlot(slot,item)
+  local slot_link = GetInventoryItemLink("player",slot)
+  if slot_link then
+    local _,_,slotItem,_ = string.find(slot_link,"|h%[?([^%[%]]*)%]|h")
+    local s1,s2 = string.lower(ItemLinkToName(slotItem)),string.lower(ItemLinkToName(item))
+    debug_print("checkItemSlot:"..s1)
+    debug_print("checkItemSlot:"..s2)
+    if s1 == s2 then return slot end
   end
 end
 
@@ -86,29 +101,32 @@ local function OnEvent()
         and not UnitIsDeadOrGhost("player")
         and event == "BAG_UPDATE" then
       debug_print("player inv changed")
-      local b,s,_ = FindItem(phylactryName)
+      local b,s,_ = FindBagItem(PhylactrifySettings.item_link)
       if b and s then
         PickupContainerItem(b,s)
-        EquipCursorItem(PhylactrifySettings.trink_slot)
+        EquipCursorItem(PhylactrifySettings.item_slot)
       end
       PhylactrifySettings.do_toggle = false
       if PhylactrifySettings.announce then
-        amprint(phylactryName .. " cycled off and on.")
+        amprint(PhylactrifySettings.item_link .. " cycled off and on.")
       end
     elseif event == "PLAYER_DEAD" then
       debug_print("player dead")
-      PhylactrifySettings.trink_slot = checkTrinketSlot(13,phylactryName) or checkTrinketSlot(14,phylactryName)
-      debug_print(PhylactrifySettings.trink_slot)
-      if PhylactrifySettings.trink_slot then
+      local slot = FindInvItemSlot(PhylactrifySettings.item_link)
+      debug_print(PhylactrifySettings.item_slot)
+      if slot then -- PhylactrifySettings.item_slot then
+        PhylactrifySettings.item_slot = slot
+        local link = GetInventoryItemLink("player",PhylactrifySettings.item_slot)
+        PhylactrifySettings.item_link = link
         PhylactrifySettings.do_toggle = true
-        debug_print(PhylactrifySettings.trink_slot)
+        debug_print(PhylactrifySettings.item_slot)
       end
     elseif PhylactrifySettings.do_toggle
         and checkNaxx()
         and (event == "PLAYER_UNGHOST" or event == "PLAYER_ALIVE")
         and not UnitIsDeadOrGhost("player") then
       debug_print("player not dead")
-      PickupInventoryItem(PhylactrifySettings.trink_slot)
+      PickupInventoryItem(PhylactrifySettings.item_slot)
       PutItemInBackpack()
     end
   elseif event == "ADDON_LOADED" then
@@ -153,11 +171,18 @@ local function handleCommands(msg,editbox)
   elseif args[1] == "announce" then
     PhylactrifySettings.announce = not PhylactrifySettings.announce
     amprint("Addon enabled: "..showOnOff(PhylactrifySettings.enabled))
-  else -- make group size color by if you're in a big enough group currently
-    amprint('Phylactrify: Automatically re-equip [Mark of the Champion] on death.')
+  elseif args[1] == "item" and args[2] then
+    table.remove(args,1)
+    local item = table.concat(args, " ")
+    debug_print(item)
+    PhylactrifySettings.item_link = item
+    amprint("Tracking enabled for: "..PhylactrifySettings.item_link)
+  else
+    amprint('Phylactrify: Automatically re-equip '..PhylactrifySettings.item_link..' on death.')
     amprint('- Addon [enabled]: ' .. showOnOff(PhylactrifySettings.enabled))
     amprint('- Active only in [naxx]ramas: ' .. showOnOff(PhylactrifySettings.naxx_only))
     amprint('- [announce] the swap: ' .. showOnOff(PhylactrifySettings.announce))
+    amprint('- Custom [item] to track (via name or link): ' .. PhylactrifySettings.item_link)
   end
 end
 
--- readme.md
+++ readme.md
@@ -1,8 +1,8 @@
 Phylactrify
 ===
 
-An addon to automatically re-equip [Mark of the Champion](https://database.turtle-wow.org/?item=23206) on Death  
-
+An addon to automatically re-equip an item on death.  
+[Mark of the Champion](https://database.turtle-wow.org/?item=23206) is to be toggled as the default  
 The buff this trinket provides does not re-apply when you ressurect so this addon fixes that.  
 
 `/phylactrify` in game to see some options  
